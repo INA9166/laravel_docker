eba34051990181dd4dde9ecc9d65984f45c0f0ab
INA9166 <ihihihihih123456@gmail.com>
2023-04-05 23:41:17 +0900

    修正

--- src/laravel/resources/views/login.blade.php
+++ src/laravel/resources/views/login.blade.php
@@ -2,7 +2,7 @@
     <body>
         <form name="frm" method="post" action="{{url('/loginAuth')}}">
             @csrf
-            <input type="text" name="login_id">
+            ID：<input type="text" name="login_id">
             <input type="submit" value="ログイン">
         </form>
     </body>
