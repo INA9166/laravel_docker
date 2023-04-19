4d3a452e9e9f3ed9d24902ae2ec584299cf53dc7
INA9166 <ihihihihih123456@gmail.com>
2023-04-20 00:34:44 +0900

    修正

--- src/laravel/resources/views/login.blade.php
+++ src/laravel/resources/views/login.blade.php
@@ -1,9 +1,13 @@
 <html lang="ja">
+    <head>
+        <link href="/css/base.css" rel="stylesheet" type="text/css">
+    </head>
     <body>
         <form name="frm" method="post" action="{{url('/loginAuth')}}">
             @csrf
             ID：<input type="text" name="login_id">
             <input type="submit" value="ログイン">
         </form>
+        <img src="/images/neko.jpeg">
     </body>
 </html>
--- src/laravel/resources/views/top.blade.php
+++ src/laravel/resources/views/top.blade.php
@@ -1,4 +1,7 @@
 <html lang="ja">
+    <head>
+        <link href="/css/base.css" rel="stylesheet" type="text/css">
+    </head>
     <body>
         <p>{{$user->name}}</p>
         <p>{{request('q')}}</p>
